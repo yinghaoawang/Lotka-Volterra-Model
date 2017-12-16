bd171d892c20d100edf4a4d710d4f196fe462d6d
Yinghao Wang <yinghao.a.wang@gmail.com>
2017-12-16 13:54:17 -0800

    lv sim base movement and drawing loop works

--- lv-sim.js
+++ lv-sim.js
@@ -1,4 +1,5 @@
 var LVSim = function(canvas) {
+    var canvas, ctx;
     var width, height;
     var a, b, c, d;
     var dirChangeT;
@@ -6,24 +7,74 @@ var LVSim = function(canvas) {
     var deltaT;
     var preys, predators;
     var x0, y0;
-    this.width = canvas.width();
-    this.height = canvas.height();
+    var data;
+    var frameCount;
+    var fps = 30;
+    this.ctx = canvas.getContext('2d');
+    this.width = canvas.width;
+    this.height = canvas.height;
     this.a = .25;
     this.b = .15;
     this.c = .15;
     this.d = .1;
     this.dirChangeT = 30;
-    this.predatorSize = 3;
-    this.preySize = 3;
+    this.predatorSize = 1;
+    this.preySize = 1;
     this.deltaT = 30;
     this.x0 = 300;
-    this.y0 = 100;
+    this.y0 = 100
     this.init = function() {
+        this.data = [[], []]
         this.preys = [];
         this.predators = [];
-        for (var i = 0; i < this.x0; ++i) this.preys.push(this.newRandomPrey());
+        for (var i = 0; i < this.x0; ++i) {
+            this.preys.push(this.newRandomPrey());
+        }
         for (var i = 0; i < this.y0; ++i) this.predators.push(this.newRandomPredator());
+
+        this.frameCount = 0;
+        this.draw();
     }
+    this.run = function() {
+        var self = this;
+        setInterval(function() {
+            self.update();
+            self.draw();
+        }, 1000/fps);
+    }
+    this.update = function() {
+        // move predators every frame
+        for (var i = 0; i < this.preys.length; ++i) this.preys[i].move();
+        for (var i = 0; i < this.predators.length; ++i) this.predators[i].move();
+
+        // change direction at given times
+        if (this.frameCount % this.dirChangeT) {
+            for (var i = 0; i < this.preys.length; ++i) this.preys[i].changeDirection();
+            for (var i = 0; i < this.predators.length; ++i) this.predators[i].changeDirection();
+        }
+
+        // actions for a step
+        if (this.frameCount % this.deltaT == 0) {
+            this.data[0].push(this.preys.length);
+            this.data[1].push(this.predators.length);
+        }
+    }
+    this.draw = function() {
+        // clears screen
+        this.ctx.clearRect(0, 0, this.width, this.height);
+        // draw and move predator/prey every frame
+        for (var i = 0; i < this.preys.length; ++i) {
+            this.preys[i].display(this.ctx);
+        }
+        for (var i = 0; i < this.predators.length; ++i) {
+            this.predators[i].display(this.ctx);
+        }
+
+        // increment frameCount
+        ++this.frameCount;
+        console.log(this.frameCount);
+    }
+    // helpers
     this.newRandomPrey = function() {
         return new Prey(Math.floor(Math.random() * this.width), Math.floor(Math.random() * this.height), this.preySize);
     }
@@ -32,16 +83,33 @@ var LVSim = function(canvas) {
     }
 }
 
-LVSim.prototype.newRandomPredator = function() {
-    return new Predator(Math.floor(Math.random() * this.width), Math.floor(Math.random() * this.height), this.predatorSize);
-}
-
+// classes
 class Creature {
     constructor(xPos, yPos, size) {
         this.xPos = xPos;
         this.yPos = yPos;
+        this.speed = 3;
+        this.xVel = Math.random(-1 * this.speed, this.speed);
+        this.yVel = Math.random(-1 * this.speed, this.speed);
         this.size = size;
         this.age = 0;
+        this.color = new Color(0, 0, 0);
+    }
+    move() {
+        this.xPos += this.xVel;
+        this.yPos += this.yVel;
+    }
+    // changes direction to a random direction based on speed
+    changeDirection() {
+        this.xVel = Math.random(-1 * this.speed, this.speed);
+        this.yVel = Math.random(-1 * this.speed, this.speed);
+    }
+    display(ctx) {
+        // draw circle
+        ctx.beginPath();
+        ctx.arc(this.xPos, this.yPos, this.size, 0, 2 * Math.PI);
+        ctx.fillStyle = this.color;
+        ctx.fill();
     }
 }
 
@@ -49,7 +117,7 @@ class Creature {
 class Prey extends Creature {
     constructor(xPos, yPos, size) {
         super(xPos, yPos, size);
-        this.color =  new Color(255, 80, 80);
+        this.color = new Color(255, 80, 80);
     }
 }
 class Predator extends Creature {
@@ -64,5 +132,8 @@ class Color {
         this.r = r;
         this.g = g;
         this.b = b;
+    }
+    toString() {
+        return 'rgb(' + this.r + ',' + this.g + ',' + this.b + ')';
     }
 }
